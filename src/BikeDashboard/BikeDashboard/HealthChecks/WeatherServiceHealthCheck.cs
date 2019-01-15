04dc52e8b5a93f7730de649d19ec853c1ba1f1fa
andmos <andreas.mosti@gmail.com>
2019-01-15 20:53:01 +0100

    healthcheck for weatherservice #2

---
+++ src/BikeDashboard/BikeDashboard/HealthChecks/WeatherServiceHealthCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BikeDashboard.Models;
+using BikeDashboard.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BikeDashboard.HealthChecks
+{
+	public class WeatherServiceHealthCheck : IHealthCheck
+    {
+		private readonly IWeatherService _weatherService;
+		private readonly StationCoordinates _testableCoordinates;
+		private const double _latitude = 40.730610; //New York City
+		private const double _longitude = -73.935242; 
+       
+		public WeatherServiceHealthCheck(IWeatherService weatherService)
+        {
+			_weatherService = weatherService;
+			_testableCoordinates = new StationCoordinates { Latitude = _latitude, Longitude = _longitude };
+		}
+
+		public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+		{
+			try
+			{
+				var forcast = await _weatherService.GetDailyForeCastAsync(_testableCoordinates);
+
+				if(forcast.Forecasts.Any())
+				{
+					return HealthCheckResult.Healthy();
+				}
+				return HealthCheckResult.Degraded();
+			}
+			catch(Exception ex)
+			{
+				return HealthCheckResult.Degraded(nameof(IWeatherService), ex);
+			}
+		}
+	}
+}
--- src/BikeDashboard/BikeDashboard/Startup.cs
+++ src/BikeDashboard/BikeDashboard/Startup.cs
@@ -5,6 +5,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BikeDashboard.Services;
 using BikeDashboard.HealthChecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Mime;
 
 namespace BikeDashboard
 {
@@ -27,13 +34,17 @@ namespace BikeDashboard
             services.AddOptions();
 
             var gbfsAddress = Configuration.GetValue<string>("GBFSAddress");
-            var bikeClient = new Client(gbfsAddress);
-			var weatherService = new WeatherService(Configuration.GetValue<string>("WeatherServiceAPIKey"));
-			services.AddSingleton<IBikeshareClient>(bikeClient);
-			services.AddSingleton<IWeatherService>(weatherService);
+			IBikeshareClient bikeClient = new Client(gbfsAddress);
+			IWeatherService weatherService = new WeatherService(Configuration.GetValue<string>("WeatherServiceAPIKey"));
+			services.AddSingleton(bikeClient);
+			services.AddSingleton(weatherService);
 			services.AddSingleton<IStationService>(new StationService(bikeClient, Configuration.GetValue<string>("StationName")));
 
-            services.AddHealthChecks().AddCheck<BikeshareClientHealthCheck>(gbfsAddress);
+			services.AddHealthChecks().AddCheck<BikeshareClientHealthCheck>(nameof(bikeClient));
+			if(weatherService.FeatureEnabled)
+			{
+				services.AddHealthChecks().AddCheck<WeatherServiceHealthCheck>(nameof(weatherService));
+			}
         }
 
         public IConfiguration Configuration { get; }
@@ -53,7 +64,9 @@ namespace BikeDashboard
 
             app.UseStaticFiles();
 
-            app.UseHealthChecks("/api/health");
+			var healthCheckOptions = CreateHealthCheckOptions();
+			app.UseHealthChecks("/api/health", healthCheckOptions);
+
             app.UseMvc(routes =>
             {
 			routes.MapRoute(
@@ -64,5 +77,29 @@ namespace BikeDashboard
                     template: "{controller}/{action=Index}/{id?}");
             });
         }
-    }
+
+		private HealthCheckOptions CreateHealthCheckOptions()
+		{
+			var options = new HealthCheckOptions();  
+			options.ResultStatusCodes[HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable;
+			options.ResultStatusCodes[HealthStatus.Degraded] = StatusCodes.Status206PartialContent;
+
+            options.ResponseWriter = async (ctx, rpt) =>  
+            {  
+                var result = JsonConvert.SerializeObject(new  
+                {  
+                    status = rpt.Status.ToString(),  
+					checks = rpt.Entries.Select(e => new { service = e.Key, status = Enum.GetName(typeof(HealthStatus), e.Value.Status), error = e.Value.Exception?.Message })
+                },
+                Formatting.None,new JsonSerializerSettings()  
+                {  
+                    NullValueHandling = NullValueHandling.Ignore  
+                });  
+                
+				ctx.Response.ContentType = MediaTypeNames.Application.Json;  
+                await ctx.Response.WriteAsync(result);  
+            };
+			return options;
+		}
+	}
 }
