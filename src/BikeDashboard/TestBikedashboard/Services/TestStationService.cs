919b6c2612c86def7514e456e5f262245a1ec513
andmos <andreas.mosti@gmail.com>
2019-03-09 15:49:58 +0100

    established testproject for #18

---
+++ src/BikeDashboard/TestBikedashboard/Services/TestStationService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using BikeDashboard.Services;
+using BikeshareClient;
+using System.Linq;
+using TestBikedashboard.Helpers;
+
+namespace TestBikedashboard.Services
+{
+    public class TestStationService
+    {
+
+        private readonly string _defaultFavoriteStation;
+        private readonly IBikeshareClient _bikeshareClientStub;
+
+        public TestStationService()
+        {
+            _defaultFavoriteStation = "Ilevollen";
+            _bikeshareClientStub = new TestableBikeshareClient();
+        }
+
+        [Fact]
+        public async Task GetFavoriteStation_GivenValidDefaultStationName_ReturnFavoriteStation()
+        {
+            var cut = new StationService(_bikeshareClientStub, _defaultFavoriteStation);
+
+            var station = await cut.GetFavoriteStation();
+
+            Assert.Equal(_defaultFavoriteStation, station.Name);
+        }
+
+        [Fact]
+        public async Task GetFavoriteStation_GivenValidStationNameParameter_ReturnsFavoriteStation()
+        {
+            var cut = new StationService(_bikeshareClientStub, _defaultFavoriteStation);
+            var expectedStation = "Strandveikaia";
+
+            var station = await cut.GetFavoriteStation(expectedStation);
+
+            Assert.Equal(expectedStation, station.Name);
+        }
+
+        [Fact]
+        public async Task GetAvailableStations_ReturnsAllAvialableStation()
+        {
+            var cut = new StationService(_bikeshareClientStub, _defaultFavoriteStation);
+
+            var stations = await cut.GetAllAvailableStations();
+
+            Assert.Equal(50, stations.Count());
+        }
+
+        [Fact]
+        public async Task GetClosestAvailableStation_GivenValidStation_ReturnsClosestStationWithRentingBikes()
+        {
+            var cut = new StationService(_bikeshareClientStub, _defaultFavoriteStation);
+            var favoriteStation = await cut.GetFavoriteStation("Skansen");
+            var expectedStationName = "Ilaparken";
+
+            var closestStation = await cut.GetClosestAvailableStation(favoriteStation);
+
+            Assert.Equal(expectedStationName, closestStation.Name);
+        }
+    }
+}
---
+++ src/BikeDashboard/TestBikedashboard/Stubs/TestableBikeshareClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using BikeshareClient;
+using BikeshareClient.Models;
+using Newtonsoft.Json;
+using TestBikedashboard.DTO;
+
+namespace TestBikedashboard.Helpers
+{
+    public class TestableBikeshareClient : IBikeshareClient
+    {
+
+        public Task<IEnumerable<Feed>> GetAvailableFeedsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Language>> GetAvailableLanguagesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<BikeStatus>> GetBikeStatusAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Station>> GetStationsAsync()
+        {
+            var stationsJson = await File.ReadAllTextAsync("TestData/BikeStations.json");
+            var dto = JsonConvert.DeserializeObject<StationDTO>(stationsJson);
+            return dto.StationsData.Stations;
+        }
+
+        public async Task<IEnumerable<StationStatus>> GetStationsStatusAsync()
+        {
+            var stationStatusJson = await File.ReadAllTextAsync("TestData/BikeStationsStatus.json");
+            var dto = JsonConvert.DeserializeObject<StationStatusDTO>(stationStatusJson);
+            return dto.StationsStatusData.StationsStatus;
+        }
+
+        public Task<SystemInformation> GetSystemInformationAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
