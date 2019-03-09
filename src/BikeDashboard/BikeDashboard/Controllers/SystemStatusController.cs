9d8c1c21b829d1fe7485bf3a93dd60869c036285
andmos <andreas.mosti@gmail.com>
2019-03-09 17:58:56 +0100

    Added controller for system status

---
+++ src/BikeDashboard/BikeDashboard/Controllers/SystemStatusController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BikeDashboard.Models;
+using BikeshareClient;
+using System.Linq;
+
+namespace BikeDashboard.Controllers
+{
+    [Route("api/[controller]")]
+    public class SystemStatusController : Controller
+    {
+        private readonly IBikeshareClient _bikeshareClient;
+
+        public SystemStatusController(IBikeshareClient bikeshareClient)
+        {
+            _bikeshareClient = bikeshareClient;
+        }
+
+        [HttpGet]
+        public async Task<SystemStatus> GetAsync()
+        {
+            var allStations = await _bikeshareClient.GetStationsAsync();
+            var stationsWithBikes = await _bikeshareClient.GetStationsStatusAsync();
+            var systemInfo = await _bikeshareClient.GetSystemInformationAsync();
+
+            return new SystemStatus
+            { 
+                SystemName = systemInfo.Name, 
+                SystemOperator = systemInfo.OperatorName, 
+                Stations = allStations.Count(), 
+                StationsWithAvailableBikes = stationsWithBikes.Count(s => s.BikesAvailable > 0),
+                StationsWithAvailableLocks = stationsWithBikes.Count(s => s.DocksAvailable > 0) 
+            };
+
+        }
+    }
+}
---
+++ src/BikeDashboard/BikeDashboard/Models/SystemStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BikeDashboard.Models
+{
+    public class SystemStatus
+    {
+        public string SystemName { get; set; }
+        public string SystemOperator { get; set; }
+        public int Stations { get; set; }
+        public int StationsWithAvailableBikes { get; set; }
+        public int StationsWithAvailableLocks { get; set; }
+    }
+}
