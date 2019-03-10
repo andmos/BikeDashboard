5c203fdbc78b6b2a0a303701826d816268c0c9f9
andmos <andreas.mosti@gmail.com>
2019-03-10 18:20:32 +0100

    more controller tests

---
+++ src/BikeDashboard/TestBikedashboard/Controllers/TestSystemStatusController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BikeDashboard;
+using BikeDashboard.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace TestBikedashboard.Controllers
+{
+    public class TestSystemStatusController : IClassFixture<BikeDashboardCustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public TestSystemStatusController(BikeDashboardCustomWebApplicationFactory<Startup> factory) 
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAsync_ReturnsSystemStatus() 
+        {
+            var response = await _client.GetAsync("/api/SystemStatus");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var systemStatus = JsonConvert.DeserializeObject<SystemStatus>(content);
+
+            Assert.Equal("Test Bike Provider", systemStatus.SystemName);
+        }
+    }
+}
