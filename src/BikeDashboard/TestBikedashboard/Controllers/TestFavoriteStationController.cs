d04c692d2410195212f7aa31b1ca3768547ade7d
andmos <andreas.mosti@gmail.com>
2019-03-16 14:12:42 +0100

    more controller tests

---
+++ src/BikeDashboard/TestBikedashboard/Controllers/TestFavoriteStationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BikeDashboard.DTO;
+using Newtonsoft.Json;
+using Xunit;
+using System.Linq;
+
+namespace TestBikedashboard.Controllers
+{
+    public class TestFavoriteStationController : TestBase
+    {
+        private readonly HttpClient _client;
+
+        public TestFavoriteStationController()
+        {
+            _client = Factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_GivenCorrectConfiguration_ReturnsStationWeatherDTO() 
+        {
+            var response = await _client.GetAsync("/api/FavoriteStation");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var station = JsonConvert.DeserializeObject<StationWeatherDTO>(content);
+
+            Assert.Equal(DefaultStation, station.Station.Name);
+        }
+
+        [Fact]
+        public async Task Get_GivenValidStationAsQueryString_ReturnsStationWeatherDTO()
+        {
+            var response = await _client.GetAsync("/api/FavoriteStation?stationName=skansen");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var station = JsonConvert.DeserializeObject<StationWeatherDTO>(content);
+
+            Assert.Equal("Skansen", station.Station.Name);
+        }
+
+        [Fact]
+        public async Task GetAsync_GivenInvalidStationQuery_ReturnsDefaultStation()
+        {
+            var response = await _client.GetAsync("/api/FavoriteStation?stationName=skanseeeen");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var station = JsonConvert.DeserializeObject<StationWeatherDTO>(content);
+
+            Assert.Equal(DefaultStation, station.Station.Name);
+        }
+    }
+}
---
+++ src/BikeDashboard/TestBikedashboard/Controllers/TestStationsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BikeDashboard.Models;
+using Newtonsoft.Json;
+using Xunit;
+using System.Linq;
+
+namespace TestBikedashboard.Controllers
+{
+    public class TestStationsController : TestBase
+    {
+
+        private readonly HttpClient _client;
+
+
+        public TestStationsController() 
+        {
+            _client = Factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsStations() 
+        {
+            var response = await _client.GetAsync("/api/Stations");
+
+            var content = await response.Content.ReadAsStringAsync();
+            var stations = JsonConvert.DeserializeObject<IEnumerable<FavoriteStation>>(content);
+
+            Assert.True(stations.Count() > 1);
+        }
+    }
+}
