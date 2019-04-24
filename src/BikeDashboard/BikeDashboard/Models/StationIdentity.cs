7c641c5e5540c293512178b8ee590dcff1b5280d
andmos <andreas.mosti@gmail.com>
2019-04-24 19:37:15 +0200

    small refactor, upgrading bikeshareclient, added properties to systemstatus api

---
+++ src/BikeDashboard/BikeDashboard/Models/StationIdentity.cs
@@ -0,0 +1,14 @@
+﻿namespace BikeDashboard.Models
+{
+    public class StationIdentity
+    {
+        public StationIdentity(string name, string id)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+    }
+}
