edb3f8e54885201ac00a0da962a9df970de29762
andmos <andreas.mosti@gmail.com>
2019-05-04 12:38:07 +0200

    added timedweatherservicecache, fixes #5

---
+++ src/BikeDashboard/BikeDashboard/Services/TimeCachedWeatherService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using BikeDashboard.Models;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BikeDashboard.Services
+{
+    public class TimeCachedWeatherService : IWeatherService
+    {
+        private const double CachedMinutes = 10;
+        private readonly IWeatherService _weatherService;
+        private readonly IMemoryCache _memoryCache;
+
+        public TimeCachedWeatherService(IWeatherService weatherService, IMemoryCache memoryCache)
+        {
+            _weatherService = weatherService;
+            _memoryCache = memoryCache;
+        }
+
+        public bool FeatureEnabled => _weatherService.FeatureEnabled;
+
+        public async Task<WeatherForecastReport> GetDailyForeCastAsync(StationCoordinates coordinates)
+        {
+            var forcastReport = await _memoryCache.GetOrCreateAsync((coordinates.Latitude, coordinates.Longitude).GetHashCode(), async report => 
+            {
+                report.SetAbsoluteExpiration(TimeSpan.FromMinutes(CachedMinutes));
+                return await _weatherService.GetDailyForeCastAsync(coordinates);
+
+            });
+            return forcastReport;
+        }
+    }
+}
---
+++ src/BikeDashboard/TestBikedashboard/Extensions/TestTimeCachedWeatherService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BikeDashboard.Services;
+using Xunit;
+using TestBikedashboard.Stubs;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using BikeDashboard.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBikedashboard.Extensions
+{
+    public class TestTimeCachedWeatherService
+    {
+        [Fact]
+        public async Task GetDailyForeCastAsync_GivenStationCoordinates_ReturnsWeatherForcast() 
+        {
+            var cut = CreateClassUnderTest();
+            var testCoordinates = new StationCoordinates { Latitude = 100, Longitude = 200 };
+
+            var weatherForcast = await cut.GetDailyForeCastAsync(testCoordinates);
+
+            Assert.Equal(2.0, weatherForcast.Forecasts.FirstOrDefault().Rain.Rainfall);
+        }
+
+        [Fact] 
+        public async Task GetDailyForeCastAsync_GivenSameStationCoordinates_ReturnsSameInstance() 
+        {
+            var cut = CreateClassUnderTest();
+            var testCoordinates = new StationCoordinates { Latitude = 100, Longitude = 200 };
+
+            var weatherForcast1 = await cut.GetDailyForeCastAsync(testCoordinates);
+            var weatherForcast2 = await cut.GetDailyForeCastAsync(testCoordinates);
+
+            Assert.Same(weatherForcast1, weatherForcast2);
+        }
+
+        private TimeCachedWeatherService CreateClassUnderTest() 
+        {
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+            var serviceProvider = services.BuildServiceProvider();
+            var memoryCache = serviceProvider.GetService<IMemoryCache>();
+
+            var weatherServiceStub = new TestableWeatherService();
+
+            return new TimeCachedWeatherService(weatherServiceStub, memoryCache);
+        }
+    }
+}
